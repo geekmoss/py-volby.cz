bc82142477bcd7adaebd1fb3ddfb05064e31214b
Geekmoss <jakub.janecek@fw-fw.cz>
2021-10-09 01:16:01 +0200

    Add UserAgent

--- volby_cz/calls.py
+++ volby_cz/calls.py
@@ -6,7 +6,7 @@ import requests
 
 
 def get_chamber_of_deputies_election_results(elections: Union[ChamberOfDeputiesElectionEnum, str]) -> Ps:
-    res = requests.get(f'https://volby.cz/pls/{elections}/vysledky')
+    res = requests.get(f'https://volby.cz/pls/{elections}/vysledky', headers={'User-Agent': 'py-volby.cz'})
     res.raise_for_status()
 
     return Ps.from_xml_node(cElementTree.fromstring(res.content))
